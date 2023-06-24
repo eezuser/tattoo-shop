cf38160a3fd6afaead45197a4b91bb17ae7b68cd
waleola965 <waleola965@yahoo.com>
2023-06-24 12:45:47 -0400

    Added conditionals and instantiated the Customer class

--- main.py
+++ main.py
@@ -16,26 +16,29 @@ class Artist:
         self.level = level 
         self.name = name  
 
- 
-print('Welcome to Petes Tattoo Shop', 'what\'s your name?')
-
-cust1 = Customer()
-
-#print("Here are the Artists today, please choose one.")
-
 names = []
 for person in artists:
     names = []
     names.append(list(artists.keys()))
-
+ 
+print('Welcome to Pete\'s Tattoo Shop')
+print('Here is a list of the Artists')
 print(names)
 
+cust1 = Customer()
 
-#if 'mean' in user_input:
-  #  print(artists['John'])
-#elif user_input == 'so-so':
-   # print(artists['Kerry'])
-#else:
-   # print('You did not choose anyone but here are you options:', artists.())
+if cust1.attitude == 'Bad' or 'mean' or 'bad' or 'Mean':
+    lowest_skill = names[0][0]
+    print( 'Alright', cust1.name, 'it looks like you have a', cust1.attitude, 'attitude, so', lowest_skill, 'will be your Artist today.')
+elif cust1.attitude == 'good' or 'Good' or 'fine':
+    good_skill = names[2][2]
+    print("Gotcha covered,", good_skill, "is a good choice.")
+else:
+    medium_skill = names[1][2]
+    print('Cant make a choice?, no worries', medium_skill, 'is a solid Artist.')
+
+
+
+
 
 
